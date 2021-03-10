5492b1ec2259ede1155cc6bb7e340d8af353f5bd
andrewwyh <yehim.wong@gmail.com>
2021-03-10 15:30:09 +0800

    Kiosk.service auto restart. Kiosk.sh improve autoplay

--- kiosk.sh
+++ kiosk.sh
@@ -6,11 +6,21 @@ xset -dpms
 unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-chromium-browser --noerrdialogs --disable-infobars  --kiosk play.html --hide-scrollbars &
+chromium-browser --noerrdialogs --disable-infobars  --kiosk /home/pi/vimeo-for-pi/play.html --hide-scrollbars &
 
-sleep 5
-xdotool keydown Tab
-xdotool keyup Tab
+#sleep 20
+#xdotool keydown Tab;
+#xdotool keyup Tab;
+#sleep 1
+#xdotool keydown space; xdotool keyup space;
+
+while true; do
+ xdotool keydown ctrl+r; xdotool keyup ctrl+r;
+ sleep 20
+xdotool keydown Tab;
+xdotool keyup Tab;
 sleep 1
-xdotool keydown space
-xdotool keyup space
+xdotool keydown space; xdotool keyup space;
+sleep 600
+done
+
